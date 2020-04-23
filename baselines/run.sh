b7a19d9e0f3a9a6759d97ed50c51dc07f4104061
Sarah Wiegreffe <swiegreffe6@gatech.edu>
2020-04-23 16:12:51 -0400

    add code used to produce results for A2C

--- baselines/run.sh
+++ baselines/run.sh
@@ -9,3 +9,24 @@ time python -m baselines.run --alg=deepq --env=Acrobot-v1 --log_path=logs/Acrobo
 #python -m baselines.deepq.experiments.train_mountaincar
 # python -m baselines.deepq.experiments.train_acrobot
 # python plot.py
+
+## Sarah's experiments (for CartPole):
+# atari (DONE): time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_0001_32 --save_path=models/CartPole_0001_32.pkl --num_timesteps=1e7 --lr=0.0001 --nsteps=32 # --num_env= 
+# atari_two (DONE): time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_0005_32 --save_path=models/CartPole_0005_32.pkl --num_timesteps=1e7 --lr=0.0005 --nsteps=32
+# atari_three (REDO): CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_001_32 --save_path=models/CartPole_001_32.pkl --num_timesteps=1e7 --lr=0.001 --nsteps=32
+# atari_four: CUDA_VISIBLE_DEVICES=0 time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_005_32 --save_path=models/CartPole_005_32.pkl --num_timesteps=1e7 --lr=0.005 --nsteps=32
+# atari_five: CUDA_VISIBLE_DEVICES=0 time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_0001_256 --save_path=models/CartPole_0001_256.pkl --num_timesteps=1e7 --lr=0.0001 --nsteps=256
+# atari_six: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_0005_256 --save_path=models/CartPole_0005_256.pkl --num_timesteps=1e7 --lr=0.0005 --nsteps=256
+# atari_seven: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_001_256 --save_path=models/CartPole_001_256.pkl --num_timesteps=1e7 --lr=0.001 --nsteps=256
+# atari_eight: CUDA_VISIBLE_DEVICES=0 time python -m baselines.run --alg=a2c --env=CartPole-v0 --log_path=logs/CartPole_005_256 --save_path=models/CartPole_005_256.pkl --num_timesteps=1e7 --lr=0.005 --nsteps=256
+
+# Second set (for Acrobot): 
+
+# atari: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_0001_32 --save_path=models/Acrobot_0001_32.pkl --num_timesteps=1e7 --lr=0.0001 --nsteps=32 # --num_env= 
+# atari_two: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_0005_32 --save_path=models/Acrobot_0005_32.pkl --num_timesteps=1e7 --lr=0.0005 --nsteps=32
+# atari_three: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_001_32 --save_path=models/Acrobot_001_32.pkl --num_timesteps=1e7 --lr=0.001 --nsteps=32
+# atari_four: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_005_32 --save_path=models/Acrobot_005_32.pkl --num_timesteps=1e7 --lr=0.005 --nsteps=32
+# atari_five: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_0001_256 --save_path=models/Acrobot_0001_256.pkl --num_timesteps=1e7 --lr=0.0001 --nsteps=256
+# atari_six: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_0005_256 --save_path=models/Acrobot_0005_256.pkl --num_timesteps=1e7 --lr=0.0005 --nsteps=256
+# atari_seven: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_001_256 --save_path=models/Acrobot_001_256.pkl --num_timesteps=1e7 --lr=0.001 --nsteps=256
+# atari_eight: CUDA_VISIBLE_DEVICES=1 time python -m baselines.run --alg=a2c --env=Acrobot-v1 --log_path=logs/Acrobot_005_256 --save_path=models/Acrobot_005_256.pkl --num_timesteps=1e7 --lr=0.005 --nsteps=256
